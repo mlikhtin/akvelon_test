4c74d135522cc84e91ef096ea4bfa48bda2905a8
Marat Likhtin <mlikhtin@informatica.com>
2021-06-19 10:04:36 +0300

    initial

---
+++ ClassLibrary1/Sort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Sort
+    {
+        public static void BubbleSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿//using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] numbers = new int[10];
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = rnd.Next(1, 100);
+            Console.WriteLine("Array to be sorted: ");
+            foreach (var number in numbers)
+                Console.Write(number + " ");
+            Console.WriteLine();
+ //           Sort.BubbleSort(numbers);
+            Console.WriteLine("Array is sorted: ");
+            foreach (var number in numbers)
+                Console.Write(number + " ");
+            Console.WriteLine();
+        }
+    }
+}
